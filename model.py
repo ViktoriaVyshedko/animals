ee775f15db1fd4505a12818933a14168a88b2161
Вышедко Виктория Евгеньевна <147328308+ViktoriaVyshedko@users.noreply.github.com>
2025-07-14 12:14:36 +0500

    Update model.py

--- model.py
+++ model.py
@@ -7,8 +7,8 @@ class SimpleCNN(nn.Module):
     def __init__(self, num_classes=10, input_size=224):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 32, 3, 1, 1)
-        #self.conv2 = nn.Conv2d(32, 64, 3, 1, 1)
-        #self.conv3 = nn.Conv2d(64, 128, 3, 1, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1, 1)
+        self.conv3 = nn.Conv2d(64, 128, 3, 1, 1)
         self.pool = nn.MaxPool2d(2, 2)
         
         # Рассчитываем размер после свёрточных слоёв
@@ -20,7 +20,7 @@ class SimpleCNN(nn.Module):
     
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
-        #x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv2(x)))
         x = torch.flatten(x, 1)  # [batch, channels*height*width]
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
